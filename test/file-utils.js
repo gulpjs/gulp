271834fb581f5d7859cb32ba6dae1c6dfca5a336
Contra <contra@maricopa.edu>
2013-07-05 14:35:15 -0700

    file base paths from glob-stream

--- test/file-utils.js
+++ test/file-utils.js
@@ -7,12 +7,14 @@ describe('gulp file utilities', function() {
   describe('readFile()', function() {
     it('should return a valid file struct', function(done) {
       var fname = join(__dirname, "./fixtures/test.coffee");
-      gulp.readFile(fname, function(err, struct) {
+      gulp.readFile({path:fname,base:fname}, function(err, struct) {
         should.not.exist(err);
         should.exist(struct);
         should.exist(struct.path);
         should.exist(struct.contents);
+        should.exist(struct.base);
         struct.path.should.equal(fname);
+        struct.base.should.equal(fname);
         String(struct.contents).should.equal("this is a test");
         done();
       });
