1fc3a661a4fa2d64f4fcaed9a6dc546ebafd0fba
Contra <contra@maricopa.edu>
2013-07-06 11:42:49 -0700

    watching works

--- test/watch.js
+++ test/watch.js
@@ -12,21 +12,21 @@ describe('gulp watch', function() {
   describe('watch()', function() {
     it('should return a valid file struct', function(done) {
       var fname = join(__dirname, "./fixtures/**/*.coffee");
+      fs.writeFileSync(expectedName, "testing");
 
       var watcher = gulp.watch(fname);
       watcher.on('change', function(evt) {
         should.exist(evt);
         should.exist(evt.path);
         should.exist(evt.type);
-        // dont check type for now it always seems to be messed up
-        //evt.type.should.equal('added');
+        evt.type.should.equal('changed');
         evt.path.should.equal(expectedName);
         rimraf.sync(expectedName);
         done();
       });
       setTimeout(function(){
-        fs.writeFileSync(expectedName, "test");
-      }, 200);
+        fs.writeFileSync(expectedName, "test test");
+      }, 100);
     });
   });
 });
