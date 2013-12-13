7be5e2f1b5a06bbcb459d0b2e944706b1b79a17d
Will O'Brien <will.obrien@dibit.co>
2013-12-13 16:08:13 -0500

    Allow file contents to be strings
    Some gulp plugins (eg. gulp-coffee) replace the file contents with a
    transpiled string. Fix is to write strings as you would a buffer.

--- test/dest.js
+++ test/dest.js
@@ -3,6 +3,7 @@ var should = require('should');
 var join = require('path').join;
 var rimraf = require('rimraf');
 var fs = require('fs');
+var es = require('event-stream');
 
 require('mocha');
 
@@ -14,6 +15,80 @@ describe('gulp output stream', function() {
       should.exist(stream);
       should.exist(stream.on);
       done();
+    });
+
+    it('should be able to write buffered content', function(done){
+      var outpath = join(__dirname, "./out-fixtures");
+      rimraf(outpath, function(err){
+        should.not.exist(err);
+        var instream = gulp.src(join(__dirname, "./fixtures/**/*.txt"));
+        var bufferifyContents = es.through(
+          function(file){
+            file.contents = new Buffer(file.contents);
+            this.emit('data', file);
+          },
+          function(){ this.emit('end'); }
+        );
+        var outstream = gulp.dest(outpath);
+        instream
+          .pipe(bufferifyContents)
+          .pipe(outstream);
+
+        outstream.on('error', done);
+        outstream.on('data', function(file) {
+          // data should be re-emitted right
+          should.exist(file);
+          should.exist(file.path);
+          should.exist(file.contents);
+          join(file.path,'').should.equal(join(__dirname, "./fixtures/copy/example.txt"));
+          String(file.contents).should.equal("this is a test");
+        });
+        outstream.on('end', function() {
+          fs.readFile(join(outpath, "copy", "example.txt"), function(err, contents){
+            should.not.exist(err);
+            should.exist(contents);
+            String(contents).should.equal("this is a test");
+            done();
+          });
+        });
+      });
+    });
+
+    it('should be able to write string content', function(done){
+      var outpath = join(__dirname, "./out-fixtures");
+      rimraf(outpath, function(err){
+        should.not.exist(err);
+        var instream = gulp.src(join(__dirname, "./fixtures/**/*.txt"));
+        var stringifyContents = es.through(
+          function(file){
+            file.contents = new String(file.contents);
+            this.emit('data', file);
+          },
+          function(){ this.emit('end'); }
+        );
+        var outstream = gulp.dest(outpath);
+        instream
+          .pipe(stringifyContents)
+          .pipe(outstream);
+
+        outstream.on('error', done);
+        outstream.on('data', function(file) {
+          // data should be re-emitted right
+          should.exist(file);
+          should.exist(file.path);
+          should.exist(file.contents);
+          join(file.path,'').should.equal(join(__dirname, "./fixtures/copy/example.txt"));
+          String(file.contents).should.equal("this is a test");
+        });
+        outstream.on('end', function() {
+          fs.readFile(join(outpath, "copy", "example.txt"), function(err, contents){
+            should.not.exist(err);
+            should.exist(contents);
+            String(contents).should.equal("this is a test");
+            done();
+          });
+        });
+      });
     });
 
     it('should return a output stream that writes files', function(done) {
