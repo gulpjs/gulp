b2d8b2e906f5370f090ffafc0ecec60f23fd5e7f
Nicolas Froidure <nicolas_froidure@elitwork.com>
2014-01-07 16:44:03 +0100

    End dest stream only when underlying resources are closed

--- test/dest.js
+++ test/dest.js
@@ -3,6 +3,8 @@ var should = require('should');
 var join = require('path').join;
 var rimraf = require('rimraf');
 var fs = require('fs');
+var gutil = require('gulp-util');
+var Stream = require('stream');
 
 require('mocha');
 
@@ -92,6 +94,44 @@ describe('gulp output stream', function() {
             String(contents).should.equal("this is a test");
             done();
           });
+        });
+      });
+    });
+
+    it('should return a output stream only after file close', function(done) {
+      var outpath = join(__dirname, "./out-fixtures");
+      rimraf(outpath, function(err){
+        should.not.exist(err);
+        var instream = new Stream.PassThrough({objectMode: true});
+        var outstream = instream.pipe(gulp.dest(outpath));
+
+        var fakeFile = new gutil.File({
+          path:'foo.txt',
+          contents: new Stream.PassThrough()
+        });
+        
+        instream.write(fakeFile);
+        instream.end();
+
+        outstream.on('error', done);
+        outstream.on('data', function(file) {
+          // data should be re-emitted right
+          should.exist(file);
+          should.exist(file.path);
+          should.exist(file.contents);
+          join(file.path,'').should.equal("foo.txt");
+        });
+        outstream.on('end', function() {
+          fs.readFileSync(join(outpath, "foo.txt"), 'utf-8')
+            .should.equal('plopplopplopplop');
+          done();
+        });
+        process.nextTick(function() {
+          fakeFile.contents.write(Buffer('plop'));
+          fakeFile.contents.write(Buffer('plop'));
+          fakeFile.contents.write(Buffer('plop'));
+          fakeFile.contents.write(Buffer('plop'));
+          fakeFile.contents.end();
         });
       });
     });
