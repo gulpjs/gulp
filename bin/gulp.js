153a85fd627291d7c42d9a057184b00feef3f344
Blaine Bublitz <blaine@iceddev.com>
2014-09-02 17:53:19 -0700

    add --tasks-json cli flag - closes #584

--- bin/gulp.js
+++ bin/gulp.js
@@ -116,6 +116,11 @@ function handleArguments(env) {
       };
       return logTasks(tree);
     }
+    if (opts.tasksJson) {
+      return console.log(
+        JSON.stringify(gulpInst.tree({ deep: true }), null, 2)
+      );
+    }
     try {
       gutil.log('Using gulpfile', chalk.magenta(tildify(env.configPath)));
       // TODO: do we care about the error/result from calling this?
