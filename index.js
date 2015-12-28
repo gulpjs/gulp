cb0d5d5eb204e9aea9ba9f6334952e5ec8202a49
zensh <admin@zensh.com>
2015-12-28 18:48:35 +0800

    refactor with thunk

--- index.js
+++ index.js
@@ -1,63 +1,35 @@
-'use strict';
+'use strict'
 
-var util = require('util');
-var Orchestrator = require('orchestrator');
-var gutil = require('gulp-util');
-var deprecated = require('deprecated');
-var vfs = require('vinyl-fs');
+var util = require('util')
+var vfs = require('vinyl-fs')
+var Tasks = require('./lib/tasks')
 
-function Gulp() {
-  Orchestrator.call(this);
+function Gulp () {
+  Tasks.call(this)
 }
-util.inherits(Gulp, Orchestrator);
 
-Gulp.prototype.task = Gulp.prototype.add;
-Gulp.prototype.run = function() {
-  // `run()` is deprecated as of 3.5 and will be removed in 4.0
-  // Use task dependencies instead
+util.inherits(Gulp, Tasks)
 
-  // Impose our opinion of "default" tasks onto orchestrator
-  var tasks = arguments.length ? arguments : ['default'];
+// Let people use this class from our instance
+Gulp.prototype.Gulp = Gulp
 
-  this.start.apply(this, tasks);
-};
-
-Gulp.prototype.src = vfs.src;
-Gulp.prototype.dest = vfs.dest;
-Gulp.prototype.watch = function(glob, opt, fn) {
+Gulp.prototype.src = vfs.src
+Gulp.prototype.dest = vfs.dest
+Gulp.prototype.watch = function (glob, opt, fn) {
   if (typeof opt === 'function' || Array.isArray(opt)) {
-    fn = opt;
-    opt = null;
+    fn = opt
+    opt = null
   }
 
   // Array of tasks given
   if (Array.isArray(fn)) {
-    return vfs.watch(glob, opt, function() {
-      this.start.apply(this, fn);
-    }.bind(this));
+    return vfs.watch(glob, opt, function () {
+      this.start.apply(this, fn)
+    }.bind(this))
   }
 
-  return vfs.watch(glob, opt, fn);
-};
+  return vfs.watch(glob, opt, fn)
+}
 
-// Let people use this class from our instance
-Gulp.prototype.Gulp = Gulp;
-
-// Deprecations
-deprecated.field('gulp.env has been deprecated. ' +
-  'Use your own CLI parser instead. ' +
-  'We recommend using yargs or minimist.',
-  console.warn,
-  Gulp.prototype,
-  'env',
-  gutil.env
-);
-
-Gulp.prototype.run = deprecated.method('gulp.run() has been deprecated. ' +
-  'Use task dependencies or gulp.watch task triggering instead.',
-  console.warn,
-  Gulp.prototype.run
-);
-
-var inst = new Gulp();
-module.exports = inst;
+var inst = new Gulp()
+module.exports = inst
