77de0b7793450a464e3e9ff8e164304611a99ccf
Ralic Lo <RalicLo@gmail.com>
2019-10-13 04:04:30 +0800

    Upgrade dependencies for better security

--- test/index.test.js
+++ test/index.test.js
@@ -8,52 +8,52 @@ describe('gulp', function() {
 
   describe('hasOwnProperty', function() {
     it('src', function(done) {
-      expect(gulp.hasOwnProperty('src')).toEqual(true);
+      expect(Object.prototype.hasOwnProperty.call(gulp, 'src')).toEqual(true);
       done();
     });
 
     it('dest', function(done) {
-      expect(gulp.hasOwnProperty('dest')).toEqual(true);
+      expect(Object.prototype.hasOwnProperty.call(gulp, 'dest')).toEqual(true);
       done();
     });
 
     it('symlink', function(done) {
-      expect(gulp.hasOwnProperty('symlink')).toEqual(true);
+      expect(Object.prototype.hasOwnProperty.call(gulp, 'symlink')).toEqual(true);
       done();
     });
 
     it('watch', function(done) {
-      expect(gulp.hasOwnProperty('watch')).toEqual(true);
+      expect(Object.prototype.hasOwnProperty.call(gulp, 'watch')).toEqual(true);
       done();
     });
 
     it('task', function(done) {
-      expect(gulp.hasOwnProperty('task')).toEqual(true);
+      expect(Object.prototype.hasOwnProperty.call(gulp, 'task')).toEqual(true);
       done();
     });
 
     it('series', function(done) {
-      expect(gulp.hasOwnProperty('series')).toEqual(true);
+      expect(Object.prototype.hasOwnProperty.call(gulp, 'series')).toEqual(true);
       done();
     });
 
     it('parallel', function(done) {
-      expect(gulp.hasOwnProperty('parallel')).toEqual(true);
+      expect(Object.prototype.hasOwnProperty.call(gulp, 'parallel')).toEqual(true);
       done();
     });
 
     it('tree', function(done) {
-      expect(gulp.hasOwnProperty('tree')).toEqual(true);
+      expect(Object.prototype.hasOwnProperty.call(gulp, 'tree')).toEqual(true);
       done();
     });
 
     it('lastRun', function(done) {
-      expect(gulp.hasOwnProperty('lastRun')).toEqual(true);
+      expect(Object.prototype.hasOwnProperty.call(gulp, 'lastRun')).toEqual(true);
       done();
     });
 
     it('registry', function(done) {
-      expect(gulp.hasOwnProperty('registry')).toEqual(true);
+      expect(Object.prototype.hasOwnProperty.call(gulp, 'registry')).toEqual(true);
       done();
     });
   });
